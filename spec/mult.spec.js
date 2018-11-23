424872e010fe276bd28b85979fed1d07d2aeee6b
Didier Alberto Martinez Garcia <dmartinezga@psl.com.co>
2018-11-23 09:46:45 -0500

    Variables added into before

--- spec/mult.spec.js
+++ spec/mult.spec.js
@@ -1,15 +1,19 @@
 const functions = require('../mathFunctions');
 
 describe('La multiplicacion de 2 numeros', () => {
-    beforeEach( () => {
+    var num1;
+    var num2;
+    beforeEach( () => {       
+        num1 = 3;
+        num2 = 5;
         console.log('Test Multiplicacion');
     });
 
     it("Multiplicacion de los numeros 3 y 5.", () => {
-        expect(functions.mult(3, 5)).toBe(15);
+        expect(functions.mult(num1, num2)).toBe(15);
     });
 
     it('Multiplicacion de los numeros 5 y 3', () => {        
-        expect(functions.mult(5, 3)).toBe(15);
+        expect(functions.mult(num2, num1)).toBe(15);
     });
 });
--- spec/resta.spec.js
+++ spec/resta.spec.js
@@ -1,16 +1,20 @@
 const functions = require('../mathFunctions');
 
 describe('La resta de 2 numeros', () => {
+    var num1;
+    var num2;
     beforeAll(( ) => {
+        num1 = 3;
+        num2 = 5;
         console.log('Test Resta');
     });
 
     it("Resta de los numeros 3 y 5.", () => {
-        expect(functions.resta(3, 5)).toBe(-2);
+        expect(functions.resta(num1, num2)).toBe(-2);
     });
 
     it('Resta de los numeros 5 y 3', () => {        
-        expect(functions.resta(5, 3)).toBe(2);
+        expect(functions.resta(num2, num1)).toBe(2);
     });
     
 });
